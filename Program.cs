b3733050b6a61b6b22bbb0d0c5830117127c9bc7
YasmimAlves01 <yasmimalvesrodrigues16@gmail.com>
2025-06-05 20:34:51 -0300

    Uma leve base de loop de funcionamento, irei tentar incrementar uma array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double pesoUrso;
+string sexoUrso;
+
+Console.WriteLine("Bem-Vindo ao sistema internacional de armazenamento de dado de urso");
+
+while (true)
+{
+
+    for (pesoUrso = 0; pesoUrso <= 250; pesoUrso++)
+    {
+        Console.WriteLine("Por favor digite o peso do urso: ");
+        pesoUrso = Convert.ToDouble(Console.ReadLine()!);
+
+        Console.WriteLine("Por favor digite o sexo do Urso");
+        Console.WriteLine("Onde F é igual a Feminino e M é igual a Masculino:");
+        sexoUrso = Console.ReadLine()!.ToUpper();
+    }
+    Console.WriteLine("Encerra");
+    break;
+}
