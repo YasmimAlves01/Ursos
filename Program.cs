2183b78171267e631a4b060b105ecfe23778568c
Gabriel <ggabrielcunha06@gmail.com>
2025-06-16 12:50:08 -0300

    corrigindo solicitações entradas de dados

--- Program.cs
+++ Program.cs
@@ -10,16 +10,12 @@
 //     for (int i = 0; i < ursos; i++)
 //     {
 //         peso[i] = Convert.ToInt32(Console.ReadLine()!);
-
-
-
 //         for (int j = 0; j < ursos; j++)
 //         {
 //             Console.WriteLine("Por favor digite o sexo do Urso");
 //             Console.WriteLine("Onde F é igual a Feminino e M é igual a Masculino:");
 //             sexoUrso[j] = Console.ReadLine()!;
 //         }
-
 //     }
 //     Console.WriteLine($"O peso dos ursos são {peso.Length}");
 //     Console.WriteLine($"O sexo dos ursos são {sexoUrso.Length}");
@@ -27,13 +23,17 @@
 //     break;
 // }
 
-using System.Collections.Generic; //lib da list
+using System.Text.RegularExpressions; //lib da list
 
+Console.Clear();
+Console.WriteLine("Bem-Vindo(a) ao sistema internacional de armazenamento de dados de ursos selvagens!\n");
 
-Console.WriteLine("Bem-Vindo ao sistema internacional de armazenamento de dado de urso");
+double maiorPeso = 0;
+char sexoMaiorPeso = ' ';
 
 string[] categoria = { "ML", "L", "M", "P", "MP" }; // array de categoria de pesos
-(int PesoMinimo, int PesoMaximo)[] intervalosPeso = {
+
+(int PesoMinimo, int PesoMaximo)[] intervalosPeso = {  
     (1, 50),
     (51, 100),
     (101, 150),
@@ -46,11 +46,61 @@ List<(double peso, char sexo)> ursos = new(); // funçao nova, cria uma lista (a
 
 while (true)
 {
-    Console.WriteLine("peso:");
-    if (!double.TryParse(Console.ReadLine(), out double peso)) continue; // função  nova, se cair na condição ele quebra o loop e volta pro inicio
+    Console.Write("Por favor, Informe o peso do urso (Kg):");
+    if (!double.TryParse(Console.ReadLine(), out double peso)) continue; // função nova, se cair na condição ele quebra o loop e volta pro inicio
 
-    if (peso <= 0 || peso > 250) // se cair nessas condições ele acaba o codigo e nao roda nada pra baixo
+    if (peso <= 0)
+        break;
+
+    if (peso > 250)
     {
         break;
     }
+
+    char sexo;
+    while (true)
+    {
+        Console.Write("Por favor, Informe o sexo(F/M) do urso: ");
+        string teste = Console.ReadLine()!.ToUpper().Trim();
+        if (teste == "M" || teste == "F")
+        {
+            sexo = teste[0];
+            break;
+        }
+        Console.WriteLine("Sexo inválido");
+    }
+
+    ursos.Add((peso, sexo));
+
+    if (peso > maiorPeso)
+    {
+        maiorPeso = peso;
+        sexoMaiorPeso = sexo;
+    }
+
+    string cat = saberCategoria(peso, intervalosPeso, categoria);
+    Console.WriteLine($"\nUrso registrado - Peso: {peso} Kg | Sexo: {sexo} | Categoria: {cat} \n");
 }
+
+Console.WriteLine("\nInformações do urso mais pesado:");
+Console.WriteLine($"Sexo: {sexoMaiorPeso}");
+Console.WriteLine($"Peso: {maiorPeso}Kg");
+
+string saberCategoria(double peso, (int min, int max)[] intervalos, string[] categorias)
+{
+    for (int i = 0; i < intervalos.Length; i++)
+    {
+        if (peso >= intervalos[i].min && peso <= intervalos[i].max)
+        {
+            return categorias[i];
+        }
+    }
+    return "Sem Categoria";
+}
+
+
+// foreach (var urso in ursos)
+// {
+//     string categoriaUrso = saberCategoria(urso.peso, intervalosPeso, categoria);
+//     Console.WriteLine($"Peso: {urso.peso} kg | Sexo: {urso.sexo} | Categoria: {categoriaUrso}");
+// }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Ursos
-Faça um programa para análise de dados coletados de ursos selvagens.  Para cada urso, receba o peso (kg) e o sexo (M/F). Finalize a coleta ao receber um peso zero, negativo ou acima de 250kg.
+# Projeto para coleta de dados de ursos selvagens
+
+## Sobre
+Um programa para análise de dados coletados de ursos selvagens.  Para cada urso, receba o peso (kg) e o sexo (M/F). Finalize a coleta ao receber um peso zero, negativo ou acima de 250kg.
+
+## Download
+Acesse o programa atraves do [LINK]()
+
+## Passo a passo
+
+### Primeiro você recebe boas vindas ao programa e é solicitado que insira o peso do urso(em Kg):
+![](img/IMAGE0001.png)
+
+### Logo após de informar o peso, é solicitado que insida o sexo do urso(F/M):
+![](img/IMAGE0002.jpg)
+
+### Depois de inserir as informações necessárias, aparece que o urso foi registrado e as informações dele como: peso, sexo, categoria 
+![](img/IMAGE0003.jpg)
